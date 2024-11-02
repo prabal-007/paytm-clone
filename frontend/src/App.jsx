ad9ffbd7b5d570b6ff946ee87e4f51f0e0a0482b
Starkk <42775517+prabal-007@users.noreply.github.com>
2024-11-02 20:51:34 +0530

    DB update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Signin from "./pages/Signin"
 import Signup from "./pages/Signup"
 import Dashboard from "./pages/Dashboard"
